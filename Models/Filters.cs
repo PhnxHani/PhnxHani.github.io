0e2ace91be588b7df6c712e503cecc3445dbb95f
PhnxHani <77558118+PhnxHani@users.noreply.github.com>
2025-07-06 22:04:44 +0200

    release

--- Models/Filters.cs
+++
@@ -1,8 +0,0 @@
-namespace LoadoutGenerator.Models
-{
-    public class Filters
-    {
-        public int MaxWeaponSlots { get; set; } = 4;
-        public HashSet<string> ExcludedWeapons { get; set; } = new();
-    }
-}
